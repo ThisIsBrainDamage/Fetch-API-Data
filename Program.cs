5bad226674cdf9244c5eb8a73872049c58089aaf
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-03-15 12:32:19 +1300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace JStoCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://fusionsidapi.herokuapp.com/api/wordle/";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+
+                
+            }
+        }
+    }
+}
