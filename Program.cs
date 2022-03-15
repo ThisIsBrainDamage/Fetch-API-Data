5c271224ebd64af43754333b727464b334937cac
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-03-15 13:36:42 +1300

    Added stuff

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Text;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace JStoCsharp
 {
@@ -11,18 +12,28 @@ namespace JStoCsharp
     {
         static void Main(string[] args)
         {
+            // Variables
+            string header;
+            string innerHeader;
             var url = "https://fusionsidapi.herokuapp.com/api/wordle/";
 
             var httpRequest = (HttpWebRequest)WebRequest.Create(url);
 
-
             var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+
+            // This uses the httpResponse and when they is used it'll run the code in the Curly Braces
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
                 var result = streamReader.ReadToEnd();
-                Console.WriteLine(result);
 
-                
+                /*
+                Console.WriteLine(JsonConvert.DeserializeObject(result));
+                */
+
+                dynamic stuff = JObject.Parse(result);
+                header = stuff.wordle;
+
+                Console.WriteLine(header);
             }
         }
     }
